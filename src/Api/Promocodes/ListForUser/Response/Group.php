f6e71c988eba138f2a84d8a6cca566777090f3c7
Антон Землянухин <anton.zemlya@gmail.com>
2022-06-29 10:39:12 +0300

    feat(api): Реализовал метод promocodes/list-for-user

---
+++ src/Api/Promocodes/ListForUser/Response/Group.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Studio15\SailPlay\SDK\Api\Promocodes\ListForUser\Response;
+
+final class Group
+{
+    /**
+     * Статус
+     *
+     * @var int|null
+     */
+    private $status;
+
+    /**
+     * Тип
+     *
+     * @var int|null
+     */
+    private $type;
+
+    /**
+     * Наименование
+     *
+     * @var string|null
+     */
+    private $name;
+
+    public function __construct(
+        ?int $status,
+        ?int $type,
+        ?string $name
+    ) {
+        $this->status = $status;
+        $this->type = $type;
+        $this->name = $name;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+}
